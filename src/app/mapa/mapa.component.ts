6fbce93df7466109c37f54176bdf984204d42f7a
dmast00 <obed.hinojosa@outlook.com>
2022-10-06 11:54:29 -0500

    Se agrego un backgroun img
    Se centro el mapa

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -14,9 +14,12 @@ export class MapaComponent implements OnInit {
   }
 
   PathId = addEventListener('click',(e)=>{
+    e.preventDefault();
     let item = e.target as Element
     let name = item.getAttribute('name')
     let id = item.getAttribute('id')
+    let a = document.createElement("a")
+    
     window.scrollTo(0,document.body.scrollHeight);    
     this.newItemEvent.emit(name)
 
