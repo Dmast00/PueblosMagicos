e919e7456cea03da39a9c55a324c60410d04a8ae
dmast00 <obed.hinojosa@outlook.com>
2022-10-05 22:19:25 -0500

    se agrego un event listener para saber el id del estado

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -12,4 +12,21 @@ export class MapaComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  PathId = addEventListener('mouseover',(e)=>{
+    let item = e.target as Element
+    let name = item.getAttribute('name')
+    let id = item.getAttribute('id')    
+    console.log(id, name)
+
+
+  })
+
+  removesize = addEventListener('mouseleave',(e)=>{
+    let item = e.target as Element
+    let name = item.getAttribute('name')
+    let id = item.getAttribute('id')    
+    console.log(id, name)
+    item.classList.remove('size')
+
+  })
 }
